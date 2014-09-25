6b3f270a1d63a488cee07ab57312d0d2d928bb64
Aleksander Piotrowski <pelotasplus@gmail.com>
2014-09-26 00:10:37 +0200

    add custom view to library

---
+++ 02-integration_with_library/library/src/main/java/pl/pelotasplus/rt_02_integration_with_library/library/LibTextView.java
@@ -0,0 +1,18 @@
+package pl.pelotasplus.rt_02_integration_with_library.library;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by alek on 25/09/14.
+ */
+public class LibTextView extends TextView {
+    public LibTextView(Context context) {
+        super(context);
+    }
+
+    public LibTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
